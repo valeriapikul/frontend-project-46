2d0f9cb584738d7a4a77c5210a76300eaa9b6376
valeria p <pikulvaleria@yandex.tu>
2024-05-08 12:43:31 +0300

    step 2

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -6,9 +6,11 @@ program
     .name('gendiff')
     .description('Compares two configuration files and shows a difference.')
     .version('0.0.1')
-    .helpOption('-h, --help', 'output usage information');
+    .helpOption('-h, --help', 'output usage information')
+    .arguments('[type]')
+    .option('-f, --format [type]', 'output format')
 
 program.command('compare')
     .option('-V, --version', 'output the version number')
-    
+
 program.parse();
